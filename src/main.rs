ba9ee1a2bc29168818a96c42661f414b676d3b41
dre4success <dre4success@live.com>
2025-09-16 09:55:19 +0100

    using a config build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = match Config::build(&args) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Problem parsing arguments: {e}");
+            process::exit(1);
+        }
+    };
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Shoul have been able to read the file");
+    println!("With text: \n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
